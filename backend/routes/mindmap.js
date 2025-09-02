a168ccaf493e4290f26719758bea40923ed722e1
satyam0777 <officialsatyam0777@gmail.com>
2025-09-02 11:33:13 +0530

    routes for - auth , mindmap , pattern , progress , stats

---
+++ backend/routes/mindmap.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { getMindmap, createNode, updateNode, deleteNode, getProblems, createProblem, updateProblem, deleteProblem ,getMindmapData} from '../controllers/mindmapController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+router.get('/', authMiddleware, getMindmap);
+
+router.post('/node', authMiddleware, createNode);
+router.put('/node/:id', authMiddleware, updateNode);
+router.delete('/node/:id', authMiddleware, deleteNode);
+
+router.get('/problems', authMiddleware, getProblems);
+router.post('/problem', authMiddleware, createProblem);
+router.put('/problem/:id', authMiddleware, updateProblem);
+router.delete('/problem/:id', authMiddleware, deleteProblem);
+router.get('/data', authMiddleware, getMindmapData);
+export default router;
+    
